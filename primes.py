84a4d61da03b8866c7a54c13d2fdd4fc3cf55123
vicmin03 <vicmin03@outlook.com>
2022-10-01 18:33:48 +0100

    Raise ValueError if argument is 0 or negative

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from unicodedata import numeric
+
+
+def is_prime(no):
+    if no == 2:
+        return True
+    for i in range (2, no):
+        if (no%i == 0):
+            return False
+    return True
+
 def primes(number_of_primes):
-    list = []
-    return list
+    if (number_of_primes > 0):
+        primes_list = []
+        current = 2
+        while len(primes_list) < number_of_primes:
+            if (is_prime(current)):
+                primes_list.append(current)
+            current += 1
+        print(number_of_primes, primes_list)
+        return primes_list
+    else:
+        raise ValueError("Enter a positive integer greater than 0")
